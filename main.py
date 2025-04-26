fd8ba9e20e1f4fb01c0275202d6e6224aa04f4f0
Arsplay <ok388615@gmail.com>
2025-04-26 12:45:30 +0300

    feat: add metod display

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ class Field:
         for i in range(size):
             self.grid.append([None] * self.size)
 
+    def display(self, show_ships=False):
+        letters = "    A B C D E F G H I J"
+        print(letters)
+        for i, row in enumerate(self.grid):
+            display_row = ""
+            for cell in row:
+                if cell is None:
+                    display_row += "O "
+                else:
+                    display_row += "■ "
+            if i + 1 != 10:
+                print(i + 1, " ", display_row)
+            else:
+                print(i + 1, "", display_row)
+
+Field(10, 1).display()
